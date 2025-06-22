04ced91dc7e406ebe6c19f0e7ef5991d0f22a0f3
Arcturus22 <amitsharma89460@gmail.com>
2025-06-23 04:13:34 +0530

    Add project files.

---
+++ IMS.UseCases/Inventories/ViewInventoriesByNameUseCase.cs
@@ -0,0 +1,18 @@
+﻿using IMS.CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.UseCases.Inventories
+{
+    // Changed from internal to public to allow access from other projects  
+    public class ViewInventoriesByNameUseCase
+    {
+        public IEnumerable<Inventory> ExecuteAsync(string name = "")
+        {
+
+        }
+    }
+}
