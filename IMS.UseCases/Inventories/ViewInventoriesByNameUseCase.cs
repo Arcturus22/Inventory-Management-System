eefed06dff669d7c30e8e4a9cdac82c9b8dae5bd
Arcturus22 <amitsharma89460@gmail.com>
2025-06-24 17:50:34 +0530

    InventoryList Completed and Added projects like Core Business and Plugins and Use cases in the Solution

--- IMS.UseCases/Inventories/ViewInventoriesByNameUseCase.cs
+++ IMS.UseCases/Inventories/ViewInventoriesByNameUseCase.cs
@@ -1,4 +1,6 @@
 ﻿using IMS.CoreBusiness;
+using IMS.UseCases.Inventories.Interfaces;
+using IMS.UseCases.PluginInterfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,11 +10,17 @@ using System.Threading.Tasks;
 namespace IMS.UseCases.Inventories
 {
     // Changed from internal to public to allow access from other projects  
-    public class ViewInventoriesByNameUseCase
+    public class ViewInventoriesByNameUseCase : IViewInventoriesByNameUseCase
     {
-        public IEnumerable<Inventory> ExecuteAsync(string name = "")
-        {
+        private readonly IInventoryRepository _inventoryRepository;
 
+        public ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository)
+        {
+            _inventoryRepository = inventoryRepository;
+        }
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name = "")
+        {
+            return await _inventoryRepository.GetInventoriesByNameAsync(name);
         }
     }
 }
